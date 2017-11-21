2e4be469d5eac4505f75eaa35e9ae0d1a9c89517
Tim McCarter <tim.b.mccarter@gmail.com>
2017-11-20 22:15:42 -0800

    removing htmlwebpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,7 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-
 module.exports = {
   entry: './src/app.js',
   output: {
     path: __dirname + '/dist',
     filename: 'app.bundle.js'
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Webpack Boilerplate',
-      minify: {
-        collapseWhitespace: true
-      },
-      hash: true,
-      template: './src/index.html'
-    })
-  ]
+  }
 }
